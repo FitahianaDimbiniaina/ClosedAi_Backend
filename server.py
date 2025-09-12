f0556832a0e32dbe2f84dcac65fe62f19570c98d
fitahiana Dimbiniaina <blackbooty956@gmail.com>
2025-09-12 08:23:11 +0300

    allowing cors\

--- server.py
+++ server.py
@@ -2,13 +2,14 @@ import logging
 from flask import Flask, request, jsonify
 from gemini_setup import get_gemini_response
 from flask_cors import CORS
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
 app = Flask(__name__)
-CORS(app, origins=["https://closedaifita.netlify.app"], supports_credentials=True)
+CORS(app, supports_credentials=True)
 
 @app.route("/chat", methods=["POST"])
 def chat():
